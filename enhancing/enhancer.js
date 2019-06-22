2ace284ee007f1b9be79b2f1d08078757c91c917
claridiva2000 <claridiva2000@gmail.com>
2019-06-22 13:17:06 -0500

    mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,19 +2,38 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  get
 };
 
 function succeed(item) {
-  return { ...item };
+  if (item.enhancement < 20) {
+    const newItem = {
+      name: item.name,
+      durability: 100,
+      enhancement: item.enhancement + 1
+    };
+    return newItem;
+  } else {
+    return Item;
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  const failItem = {...item}
+  if (failItem.enhancement < 15) {
+    failItem.durability = item.durability - 5
+  } else if (failItem.enhancement < 17) {
+      failItem.durability = item.durability - 10
+     }
+   else {
+    failItem.durability = item.durability - 10
+    failItem.enhancement = item.enhancement - 1    
+  }
+    return failItem;
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,108 @@
-const enhancer = require('./enhancer.js');
+const { repair, succeed, fail } = require('./enhancer.js');
 // test away!
+
+//enhancer.repair should make durability 100
+
+describe('enhancer.js', () => {
+  describe('repair()', () => {
+    it('restores durability to 100', () => {
+      expect(repair({ durability: 89 }).durability).toBe(100);
+      expect(repair({ durability: -89 }).durability).toBe(100);
+      expect(repair({ durability: 100 }).durability).toBe(100);
+      expect(repair({ durability: 0 }).durability).toEqual(100);
+    });
+  });
+
+  describe('succeed()', () => {
+    it('enhancement increases by 1 unless it is already at 20', () => {
+      //input
+      const item = {
+        name: 'The Cloak of Levitation',
+        durability: 55,
+        enhancement: 4
+      };
+      //expected outcome
+      const expected = {
+        name: 'The Cloak of Levitation',
+        durability: 100,
+        enhancement: 5
+      };
+
+      //name of output
+      const enhancedItem = succeed(item);
+
+      //assertion
+      expect(enhancedItem).toEqual(expected);
+    });
+  });
+
+  describe('fail()', () => {
+    it('enhancement> 17 reduce enhancement by 1 and reduce durability by 10 ', () => {
+      //input
+      const item = {
+        name: 'The Cloak of Levitation',
+        durability: 100,
+        enhancement: 18
+      };
+      //expected outcome
+      const expected = {
+        name: 'The Cloak of Levitation',
+        durability: 90,
+        enhancement: 17
+      };
+
+      //name of output
+      const failure = fail(item);
+
+      //assertion
+      expect(failure).toEqual(expected);
+    });
+
+    it('enhancement< than 17 reduce durability by 10 ', () => {
+      //input
+      const item = {
+        name: 'The Cloak of Levitation',
+        durability: 100,
+        enhancement: 16
+      };
+      //expected outcome
+      const expected = {
+        name: 'The Cloak of Levitation',
+        durability: 90,
+        enhancement: 16
+      };
+
+      //name of output
+      const failure = fail(item);
+
+      //assertion
+      expect(failure).toEqual(expected);
+    });
+
+
+    it('enhancement< than 15 reduce durability by 10 ', () => {
+      //input
+      const item = {
+        name: 'The Cloak of Levitation',
+        durability: 100,
+        enhancement: 14
+      };
+      //expected outcome
+      const expected = {
+        name: 'The Cloak of Levitation',
+        durability: 95,
+        enhancement: 14
+      };
+
+      //name of output
+      const failure = fail(item);
+
+      //assertion
+      expect(failure).toEqual(expected);
+    });
+
+
+  });
+
+  it.todo('should have value between 0-100 max for durabiity');
+});
